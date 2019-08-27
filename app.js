73c0b2a3a134145390b5828ee4a52c29cd3c9928
DanetDaniel <dnl.danet@gmail.com>
2019-08-27 18:35:59 +0300

    Update

---
+++ app.js
@@ -0,0 +1,7 @@
+// window.onload=function(){
+//     let adderOfClasses = document.querySelector(".classToggler");
+
+//     adderOfClasses.addEventListener("click", function() {
+//         adderOfClasses.classList.toggle("toggleMe");
+//     })
+// }
