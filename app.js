b0d9ae3a94e15f0445849b3770c63b214f7afdeb
DanetDaniel <dnl.danet@gmail.com>
2019-08-30 21:03:24 +0300

    Update

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ window.onload=function(){
     let bottomBar = document.querySelector(".bottom-bar");
     let nav = document.querySelector(".nav-container");
 
-    nav.classList.add("no-display");
+    // nav.classList.add("no-display");
 
     menuIcon.addEventListener("click", function(){
         topBar.classList.toggle("top-active");
         midBar.classList.toggle("middle-active");
         bottomBar.classList.toggle("bottom-active");
-        nav.classList.toggle("no-display");
-        nav.classList.toggle("fade-anim");
+        nav.classList.toggle("onScreen");
+        // nav.classList.toggle("fade-anim");
     })
 }
